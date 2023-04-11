f9ce7dfd29bb4bdd763c2b54e0aaec04b7b5e4bb
anthonypaddison <anthony@paddison.co.uk>
2023-04-11 13:42:57 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,611 @@
+<script type="text/javascript" src="/libs/font-awesome.js"></script>
+<link rel="stylesheet" href="/libs/bootstrap-3/css/bootstrap.min.css">
+<link rel="stylesheet" href="/libs/jquery-ui/jquery-ui.min.css">
+<link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+<link rel="icon" href="favicon.ico?1" type="image/x-icon" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<title>Helen and Will's Engagement</title>
+<style>
+/*#bannerimg {*/
+/*       background: url('banner.png') no-repeat center center fixed; */
+/*        -webkit-background-size: cover;*/
+/*        -moz-background-size: cover;*/
+/*        -o-background-size: cover;*/
+/*        background-size: cover;}*/
+.bottom-margin-please {
+    margin-top: 30px;
+    margin-bottom: 50px;
+}
+h1, h2, h3, h4, h5, h6 {
+    color: #1b3916;
+    font-family: 'Great Vibes', cursive;
+    line-height: 150%!important;
+}
+h1 {
+    font-size: 55px!important;
+}
+body {
+  background: linear-gradient(-90deg, #fffaf0, white);
+}
+.container {
+    margin-top: 30px!important;
+}
+
+.polaroid-images a {
+    cursor: pointer;
+    background: white;
+    display:inline-block; 
+    /*float: left;*/
+    margin: 0 15px 30px;
+    padding: 10px 10px 25px;
+    text-align: center;
+    text-decoration: none;
+    -webkit-box-shadow: 0 4px 6px rgba(0, 0, 0, .3);
+    -moz-box-shadow: 0 4px 6px rgba(0,0,0,.3);
+    box-shadow: 0 4px 6px rgba(0,0,0,.3);
+    -webkit-transition: all .15s linear;
+    -moz-transition: all .15s linear;
+    transition: all .15s linear;
+    z-index:0;
+    position:relative;
+}
+
+.polaroid-images a:after {
+    color: #333;
+    font-size: 20px;
+    content: attr(title);
+    position: relative;
+    top:15px;
+}
+
+.polaroid-images img { 
+    display: block; 
+    width: inherit; 
+}
+
+.polaroid-images a:nth-child(1n)
+{
+    -webkit-transform: rotate(-7deg);  
+    -moz-transform: rotate(-7deg); 
+    transform: rotate(-7deg); 
+}
+.polaroid-images a:nth-child(2n)
+{
+    -webkit-transform: rotate(14deg);  
+    -moz-transform: rotate(14deg); 
+    transform: rotate(14deg); 
+}
+.polaroid-images a:nth-child(3n) { 
+    -webkit-transform: rotate(-24deg);  
+    -moz-transform: rotate(-24deg); 
+    transform: rotate(-24deg); 
+}
+.polaroid-images a:nth-child(4n)
+{
+    -webkit-transform: rotate(14deg);  
+    -moz-transform: rotate(14deg); 
+    transform: rotate(14deg); 
+}
+.polaroid-images a:nth-child(5n)
+{
+    -webkit-transform: rotate(-16deg);  
+    -moz-transform: rotate(-16deg); 
+    transform: rotate(-16deg); 
+}
+.polaroid-images a:hover{
+    -webkit-transform: rotate(0deg); 
+    -moz-transform: rotate(0deg);
+        transform: rotate(0deg);
+    -webkit-transform: scale(1.2); 
+    -moz-transform: scale(1.2);
+        transform: scale(1.2);
+    z-index:10;
+    -webkit-box-shadow: 0 10px 20px rgba(0, 0, 0, .7);
+    -moz-box-shadow: 0 10px 20px rgba(0,0,0,.7);
+        box-shadow: 0 10px 20px rgba(0,0,0,.7);
+}
+.video_a{
+    -webkit-transform: rotate(0deg)!important;  
+    -moz-transform: rotate(0deg)!important; 
+    transform: rotate(0deg)!important; 
+    margin-right:15px!important;
+    margin-left:15px!important;
+}
+</style>
+<div class="container-fluid  text-center">
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="confetti"></div>
+    <div class="row" id="bannerimg">
+        <div class="col-sm-3 text-left">
+            <img src="cornerBorderLeft.png" style="max-width: 100%;"alt="Corner Border"/>
+        </div>
+        <div class="col-sm-6 text-center">
+            <h1>Will & Helen<br/>Congratulations on your engagement!</h1>
+            <h1>3rd June 2018</h1>
+        </div>
+        <div class="col-sm-3 text-right">
+            <img src="cornerBorderRight.png" style="max-width: 100%;" alt="Corner Border"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="polaroid-images" style="text-align: center!important;">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo9.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo10.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo14.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo11.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo2.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo13.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo4.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo12.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo5.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo6.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo7.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo8.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo3.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-3 col-sm-12">
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo15.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <a class="video_a" >
+                            <video width="560" height="340" controls> <source src="The video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'> 
+                                <source src="The video.mp4" type='video/ogg; codecs="theora, vorbis"'> 
+                            </video>
+                        </a>
+                    </div>
+                    <div class="col-md-3 col-sm-12">
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo16.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo17.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo18.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                        <a class="zoom" ><img height="200" class="image_modaler" src="photo19.jpg" alt="Will and Helens Engagement" title="Will and Helens Engagement" /></a>
+                    </div>
+                </div>
+            </div>        
+        </div>
+    </div>
+    <div class="row bottom-margin-please">
+
+        <div class="col-sm-12">
+            <!--width="850" height="550"-->
+        </div>
+    </div>
+</div>
+
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="/libs/popper.js"></script>
+<script type="text/javascript" src="/libs/bootstrap-3/js/bootstrap.min.js"></script>
+<script src="/libs/jquery-ui/jquery-ui.min.js"></script>
+<script type="text/javascript" src="/libs/bootbox.js"></script>
+
+<script type="text/javascript" src="/index.js"></script>
+<style>
+
+.zoom {
+    transition: transform .2s; /* Animation */
+    margin: 0 auto;
+}
+
+.zoom:hover {
+    transform: scale(300%); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+}
+.confetti {
+  width: 15px;
+  height: 15px;
+  background-color: #b5d088;
+  position: absolute;
+  left: 50%;
+  -webkit-animation: confetti 5s ease-in-out -2s infinite;
+          animation: confetti 5s ease-in-out -2s infinite;
+  -webkit-transform-origin: left top;
+          transform-origin: left top;
+}
+
+.confetti:nth-child(1) {
+    width: 13px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 5%;
+    -webkit-animation-delay: 0;
+    animation-delay: 0;
+}
+
+.confetti:nth-child(2) {
+    width: 16px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #8e7ff5;
+    left: 15%;
+    -webkit-animation-delay: -5s;
+    animation-delay: -5s;
+}
+
+.confetti:nth-child(3) {
+    width: 16px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #ef8fb8;
+    left: 25%;
+    -webkit-animation-delay: -3s;
+    animation-delay: -3s;
+}
+
+.confetti:nth-child(4) {
+    width: 13px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 35%;
+    -webkit-animation-delay: -2.5s;
+    animation-delay: -2.5s;
+}
+
+.confetti:nth-child(5) {
+    width: 16px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #8e7ff5;
+    left: 45%;
+    -webkit-animation-delay: -4s;
+    animation-delay: -4s;
+}
+
+.confetti:nth-child(6) {
+    width: 13px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #ef8fb8;
+    left: 55%;
+    -webkit-animation-delay: -6s;
+    animation-delay: -6s;
+}
+
+.confetti:nth-child(7) {
+    width: 13px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 65%;
+    -webkit-animation-delay: -1.5s;
+    animation-delay: -1.5s;
+}
+
+.confetti:nth-child(8) {
+    width: 16px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #8e7ff5;
+    left: 75%;
+    -webkit-animation-delay: -2s;
+      animation-delay: -2s;
+}
+
+.confetti:nth-child(9) {
+    width: 10px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #ef8fb8;
+    left: 85%;
+    -webkit-animation-delay: -3.5s;
+    animation-delay: -3.5s;
+}
+
+.confetti:nth-child(10) {
+    width: 14px;
+    height: 11px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 95%;
+    -webkit-animation-delay: -2.5s;
+    animation-delay: -2.5s;
+}
+.confetti:nth-child(11) {
+    width: 13px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 10%;
+    -webkit-animation-delay: -4s;
+    animation-delay: -4s;
+}
+
+.confetti:nth-child(12) {
+    width: 12px;
+    height: 17px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #8e7ff5;
+    left: 20%;
+    -webkit-animation-delay: -6s;
+    animation-delay: -6s;
+}
+
+.confetti:nth-child(13) {
+    width: 16px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #ef8fb8;
+    left: 30%;
+    -webkit-animation-delay: -7s;
+    animation-delay: -7s;
+}
+
+.confetti:nth-child(14) {
+    width: 13px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 40%;
+    -webkit-animation-delay: -6.5s;
+    animation-delay: -6.5s;
+}
+
+.confetti:nth-child(15) {
+    width: 16px;
+    height: 18px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #8e7ff5;
+    left: 50%;
+    -webkit-animation-delay: -7.5s;
+    animation-delay: -7.5s;
+}
+
+.confetti:nth-child(16) {
+    width: 13px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #ef8fb8;
+    left: 60%;
+    -webkit-animation-delay: -8.5s;
+    animation-delay: -8.5s;
+}
+
+.confetti:nth-child(17) {
+    width: 16px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 70%;
+    -webkit-animation-delay: -9.5s;
+    animation-delay: -9.5s;
+}
+
+.confetti:nth-child(18) {
+    width: 13px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #8e7ff5;
+    left: 80%;
+    -webkit-animation-delay: -9s;
+    animation-delay: -9s;
+}
+
+.confetti:nth-child(19) {
+    width: 13px;
+    height: 16px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #ef8fb8;
+    left: 90%;
+    -webkit-animation-delay: -10.5s;
+      animation-delay: -10.5s;
+}
+
+.confetti:nth-child(20) {
+    width: 16px;
+    height: 13px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 50px;
+    background-color: #b5d088;
+    left: 100%;
+    -webkit-animation-delay: -10s;
+    animation-delay: -10s;
+}
+@-webkit-keyframes confetti {
+  0% {
+    -webkit-transform: rotateZ(15deg) rotateY(0deg) translate(0, 0);
+            transform: rotateZ(15deg) rotateY(0deg) translate(0, 0);
+  }
+  25% {
+    -webkit-transform: rotateZ(5deg) rotateY(360deg) translate(-5vw, 20vh);
+            transform: rotateZ(5deg) rotateY(360deg) translate(-5vw, 20vh);
+  }
+  50% {
+    -webkit-transform: rotateZ(15deg) rotateY(720deg) translate(5vw, 60vh);
+            transform: rotateZ(15deg) rotateY(720deg) translate(5vw, 60vh);
+  }
+  75% {
+    -webkit-transform: rotateZ(5deg) rotateY(1080deg) translate(-10vw, 80vh);
+            transform: rotateZ(5deg) rotateY(1080deg) translate(-10vw, 80vh);
+  }
+  100% {
+    -webkit-transform: rotateZ(15deg) rotateY(1440deg) translate(10vw, 110vh);
+            transform: rotateZ(15deg) rotateY(1440deg) translate(10vw, 110vh);
+  }
+  35% {
+    -webkit-transform: rotateZ(5deg) rotateY(360deg) translate(-5vw, 20vh);
+            transform: rotateZ(5deg) rotateY(360deg) translate(-5vw, 20vh);
+  }
+  60% {
+    -webkit-transform: rotateZ(15deg) rotateY(720deg) translate(5vw, 60vh);
+            transform: rotateZ(15deg) rotateY(720deg) translate(5vw, 60vh);
+  }
+  80% {
+    -webkit-transform: rotateZ(5deg) rotateY(1080deg) translate(-10vw, 80vh);
+            transform: rotateZ(5deg) rotateY(1080deg) translate(-10vw, 80vh);
+  }
+  10% {
+    -webkit-transform: rotateZ(15deg) rotateY(1440deg) translate(10vw, 110vh);
+            transform: rotateZ(15deg) rotateY(1440deg) translate(10vw, 110vh);
+  }
+}
+
+@keyframes confetti {
+  0% {
+    -webkit-transform: rotateZ(15deg) rotateY(0deg) translate(0, 0);
+            transform: rotateZ(15deg) rotateY(0deg) translate(0, 0);
+  }
+  25% {
+    -webkit-transform: rotateZ(5deg) rotateY(360deg) translate(-5vw, 20vh);
+            transform: rotateZ(5deg) rotateY(360deg) translate(-5vw, 20vh);
+  }
+  50% {
+    -webkit-transform: rotateZ(15deg) rotateY(720deg) translate(5vw, 60vh);
+            transform: rotateZ(15deg) rotateY(720deg) translate(5vw, 60vh);
+  }
+  75% {
+    -webkit-transform: rotateZ(5deg) rotateY(1080deg) translate(-10vw, 80vh);
+            transform: rotateZ(5deg) rotateY(1080deg) translate(-10vw, 80vh);
+  }
+  100% {
+    -webkit-transform: rotateZ(15deg) rotateY(1440deg) translate(10vw, 110vh);
+            transform: rotateZ(15deg) rotateY(1440deg) translate(10vw, 110vh);
+  }
+}
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (Image) */
+.modal-content {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+}
+
+/* Caption of Modal Image (Image Text) - Same Width as the Image */
+#caption {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    text-align: center;
+    color: #ccc;
+    padding: 10px 0;
+    height: 150px;
+}
+
+/* Add Animation - Zoom in the Modal */
+.modal-content, #caption { 
+    animation-name: zoom;
+    animation-duration: 0.6s;
+}
+
+@keyframes zoom {
+    from {transform:scale(0)} 
+    to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+    position: absolute;
+    top: 15px;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+    color: #bbb;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+    .modal-content {
+        width: 100%;
+    }
+}
+</style>
